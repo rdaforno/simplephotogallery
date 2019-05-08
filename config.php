e8f815d193b15a0846da6f2bc55386b5e8b4bd04
rdaforno <keepcoding@gmx.net>
2019-05-08 20:46:15 +0200

    v1.3
    new features: randomize file names and protect photos with login

--- config.php
+++ config.php
@@ -4,5 +4,7 @@
   $infotext = "";                               // header text / info
   $copyright = "&copy; 2018 your_name, your_email/website";
   $download_filename = "photogallery.zip";      // leave blank to disable file download
+  $password = "";                               // leave blank to disable login (everyone can see the page)
+  $randomize_filenames = false;                 // if set to true, all image filenames will be randomized
   $collect_ipaddr = false;                      // should only be enabled for testing
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php  
   // --------------------------------------------------------------------------------------
-  // Simple photo gallery script v1.2
+  // Simple photo gallery script v1.3
   // (c) 2019 Reto Da Forno
   // --------------------------------------------------------------------------------------
 
@@ -10,6 +10,62 @@
   //error_reporting(E_ALL);
   //ini_set('display_errors', 1);
   ini_set('exif.encode_unicode', 'UTF-8');
+  
+  // login required?
+  if($password != "") {
+    session_start();
+    if(!isset($_SESSION['key'])) {
+      if(isset($_POST['key']) && ($_POST['key'] == $password)) {
+        $_SESSION['key'] = sha1($password);
+      } else {
+        // display login form
+        echo "<style>
+                input[type=password] { width: 200px; padding: 6px 12px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box; }
+                input[type=submit] { width: 80px; background-color: #4CAF50; color: white; padding: 6px 12px; margin-left: 10px; border: none; border-radius: 4px; cursor: pointer; }
+                input[type=submit]:hover { background-color: #45a049; }
+              </style>
+              <div id=loginform><center><br /><br /><form action=index.php method=post><input type=password name=key placeholder=password /> <input type=submit value=login /></form></center></div>";
+        exit();
+      }   
+    }
+  }
+  
+  // used to create the thumbnails
+  class SimpleJPEG {
+    var $image;
+    function load($filename)                  { if(getimagesize($filename)[2] == IMAGETYPE_JPEG) $this->image = imagecreatefromjpeg($filename); }
+    function save($filename, $compression=85) { imagejpeg($this->image, $filename, $compression); }
+    function getWidth()                       { return imagesx($this->image); }
+    function getHeight()                      { return imagesy($this->image); }
+    function orientationLandscape()           { return ($this->getWidth() > $this->getHeight()); }
+    function resizeToHeight($height)          { $this->resize($this->getWidth() * ($height / $this->getHeight()), $height); }
+    function resizeToWidth($width)            { $this->resize($width, $this->getheight() * ($width / $this->getWidth())); }
+    function scale($scale)                    { $this->resize($this->getWidth() * $scale / 100, $this->getheight() * $scale / 100); }
+    function resize($width, $height) {
+      $new_image = imagecreatetruecolor($width, $height);
+      imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+      $this->image = $new_image;   
+    }
+  }
+  
+  function incrementCounter($dlCounter) {
+    $counterfn = "counter";
+    if($randomize_filenames) {
+      $counterfn = "counter_".sha1("counter".$_SESSION['key']);
+    }
+    $filecontent = file_get_contents($counterfn);
+    $counter = explode(",", $filecontent);
+    if(count($counter) == 1) {
+      $counter[1] = 0;
+    }
+    if($dlCounter) {
+      $counter[1] = intval($counter[1]) + 1;
+    } else {
+      $counter[0] = intval($counter[0]) + 1;
+    }
+    file_put_contents($counterfn, implode(",", $counter));
+    return $counter[0];
+  }
   
   // check if the photo folder exists
   if(!is_dir("photo")) {
@@ -44,62 +100,101 @@
     header("Pragma: no-cache");
     header("Expires: 0");
     header("Location: ".$zipname);    // alternatively: readfile($zipname);    
-    // update stats
-    $dlcounter = file_get_contents('dlcounter');
-    $dlcounter = intval($dlcounter) + 1;
-    file_put_contents('dlcounter', strval($dlcounter));    
+    incrementCounter(true);           // update stats
     exit();
   }
   
-  // used to create the thumbnails
-  class SimpleJPEG {
-    var $image;
-    function load($filename)                  { if(getimagesize($filename)[2] == IMAGETYPE_JPEG) $this->image = imagecreatefromjpeg($filename); }
-    function save($filename, $compression=85) { imagejpeg($this->image, $filename, $compression); }
-    function getWidth()                       { return imagesx($this->image); }
-    function getHeight()                      { return imagesy($this->image); }
-    function orientationLandscape()           { return ($this->getWidth() > $this->getHeight()); }
-    function resizeToHeight($height)          { $this->resize($this->getWidth() * ($height / $this->getHeight()), $height); }
-    function resizeToWidth($width)            { $this->resize($width, $this->getheight() * ($width / $this->getWidth())); }
-    function scale($scale)                    { $this->resize($this->getWidth() * $scale / 100, $this->getheight() * $scale / 100); }
-    function resize($width, $height) {
-      $new_image = imagecreatetruecolor($width, $height);
-      imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
-      $this->image = $new_image;   
-    }
+  $itemlistfn = "itemlist";
+  $thumblistfn = "thumblist";
+  if($randomize_filenames) {
+    $itemlistfn = "itemlist_".sha1("itemlist".$_SESSION['key']);
+    $thumblistfn = "thumblist_".sha1("thumblist".$_SESSION['key']);
   }
    
   // check if thumbs exist, if not -> create thumbnails
   if(!is_dir("thumbs")) {
     mkdir("thumbs");
+    unlink($itemlistfn);
+    unlink($thumblistfn);
     $handle = opendir("photo");
-    while(false !== ($entry = readdir($handle))) {
-      if($entry != "." && $entry != "..") {
-        echo "generating thumbnail for file $entry...<br />";
+    while(false !== ($filename = readdir($handle))) {
+      if($filename != "." && $filename != "..") {
+        // filename randomization required?
+        if($randomize_filenames && (strlen($filename) != 44)) {   // to avoid rehashing (sha1 generates 40 chars)
+          $newfilename = sha1($filename.mt_rand());
+          rename("photo/$filename", "photo/$newfilename.jpg");
+          echo "file ".$filename." renamed to ".$newfilename.".jpg<br />";
+          $filename = $newfilename.".jpg";
+        }
         $image = new SimpleJPEG();
-        $image->load("photo/$entry");
+        $image->load("photo/$filename");
         // scale short side to 200px
         if($image->orientationLandscape()) {
           $image->resizeToHeight(200);
         } else {
           $image->resizeToWidth(200); 
         }
-        $image->save("thumbs/$entry");
+        $image->save("thumbs/$filename");
+        echo "thumbnail for file $filename generated<br />";
       }
     }
-    echo "thumbs generated!";
+    echo "thumbs generated!<br />";
   }
+  
+  // item list existing?
+  if(!file_exists($itemlistfn)) {
+    $itemlist = "";
+    $thumblist = "";
+    $files = scandir('photo/');
+    $filecnt = 0;
+    foreach($files as $file) {
+      if($file == '.' || $file == '..') {
+        continue;
+      }
+      $thumblist .= "<img src='thumbs/$file' onload='$(this).fadeIn()' onclick='openPhotoSwipe($filecnt)' class='thumb' />";
+      $filecnt = $filecnt + 1;
+      list($width, $height) = getimagesize("photo/$file");
+      $itemlist .= "      { src: 'photo/$file', w: $width, h: $height";
+      $exif = exif_read_data("photo/$file", 'IFD0', true);
+      $exif_info_string = "";
+      $title = pathinfo($file, PATHINFO_FILENAME);
+      if($exif !== false && !empty($exif['IFD0']['Make'])) {
+        $focal_length = "";
+        if(!empty($exif['EXIF']['FocalLength'])) {
+          $focal_length = explode("/", $exif['EXIF']['FocalLength']);
+          $focal_length = strval($focal_length[0] / $focal_length[1])."mm ";
+        }
+        if(!empty($exif['IFD0']['Title'])) {
+          $title = mb_convert_encoding($exif['IFD0']['Title'] , 'UTF-8' , 'UTF-16LE');
+        }
+        if(!empty($exif['IFD0']['Comments'])) {
+          $title .= " - ".$exif['IFD0']['Comments'];
+        }
+        if(!empty($exif['IFD0']['Keywords'])) {
+          $title .= " - ".$exif['IFD0']['Keywords'];
+        }
+        $exif_info_string = "- ". /*$exif['IFD0']['Make']." ".*/ trim($exif['IFD0']['Model']).", ".$focal_length.$exif['COMPUTED']['ApertureFNumber'].", ".$exif['EXIF']['ExposureTime']."s, ISO".$exif['EXIF']['ISOSpeedRatings'];
+      } 
+      $itemlist .= ", title: '$title $exif_info_string' },\n";
+    }
+    file_put_contents($itemlistfn, $itemlist);
+    file_put_contents($thumblistfn, $thumblist);    
+    echo "item list generated!<br />";
+    exit;
+  }
+  $itemlist = file_get_contents($itemlistfn);
+  $thumblist = file_get_contents($thumblistfn);
   
   // keep some stats
   if($collect_ipaddr) {
+    $ipaddrfn = 'ipaddr';
+    if($randomize_filenames) {
+      $ipaddrfn = "ipaddr_".sha1("ipaddr".$_SESSION['key']);
+    }
     $ip_addr = $_SERVER['REMOTE_ADDR']." ".date("Y-m-d H:i:s")."\n";
-    file_put_contents('ipaddr', $ip_addr, FILE_APPEND | LOCK_EX);
+    file_put_contents($ipaddrfn, $ip_addr, FILE_APPEND | LOCK_EX);
   }
-  $counter = file_get_contents('counter');
-  $counter = intval($counter) + 1;
-  file_put_contents('counter', strval($counter));
-  
-  $dlcounter = file_get_contents('dlcounter');
+  $visitors = incrementCounter(false);
 ?>
 <!DOCTYPE html>
 <html>
@@ -176,50 +271,6 @@
     // build items array
     var items = [
 <?php
-    // create the list of files using PHP
-    $itemlist = "";
-    $thumblist = "";
-    // if file exists, then load content
-    if(file_exists('itemlist')) {
-      $itemlist = file_get_contents('itemlist');
-      $thumblist = file_get_contents('thumblist');
-    } else
-    {
-      $files = scandir('photo/');
-      $filecnt = 0;
-      foreach($files as $file) {
-        if($file == '.' || $file == '..') {
-          continue;
-        }
-        $thumblist .= "<img src='thumbs/$file' onload='$(this).fadeIn()' onclick='openPhotoSwipe($filecnt)' class='thumb' />";
-        $filecnt = $filecnt + 1;
-        list($width, $height) = getimagesize("photo/$file");
-        $itemlist .= "      { src: 'photo/$file', w: $width, h: $height";
-        $exif = exif_read_data("photo/$file", 'IFD0', true);
-        $exif_info_string = "";
-        $title = pathinfo($file, PATHINFO_FILENAME);
-        if($exif !== false && !empty($exif['IFD0']['Make'])) {
-          $focal_length = "";
-          if(!empty($exif['EXIF']['FocalLength'])) {
-            $focal_length = explode("/", $exif['EXIF']['FocalLength']);
-            $focal_length = strval($focal_length[0] / $focal_length[1])."mm ";
-          }
-          if(!empty($exif['IFD0']['Title'])) {
-            $title = mb_convert_encoding($exif['IFD0']['Title'] , 'UTF-8' , 'UTF-16LE');
-          }
-          if(!empty($exif['IFD0']['Comments'])) {
-            $title .= " - ".$exif['IFD0']['Comments'];
-          }
-          if(!empty($exif['IFD0']['Keywords'])) {
-            $title .= " - ".$exif['IFD0']['Keywords'];
-          }            
-          $exif_info_string = "- ". /*$exif['IFD0']['Make']." ".*/ trim($exif['IFD0']['Model']).", ".$focal_length.$exif['COMPUTED']['ApertureFNumber'].", ".$exif['EXIF']['ExposureTime']."s, ISO".$exif['EXIF']['ISOSpeedRatings'];
-        } 
-        $itemlist .= ", title: '$title $exif_info_string' },\n";
-      }
-      file_put_contents('itemlist', $itemlist);
-      file_put_contents('thumblist', $thumblist);
-    }
     echo $itemlist;
 ?>
     ];
@@ -256,7 +307,7 @@ echo $thumblist;
     <br />
     <?php echo $copyright; ?><br />
   </div>
-  <?php echo $counter." ".$dlcounter; ?>
+  <?php echo $visitors; ?>
 </center>
 
 <!-- Root element of PhotoSwipe. Must have class pswp. -->
